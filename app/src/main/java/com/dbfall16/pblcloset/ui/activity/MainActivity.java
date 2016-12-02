745d6b62ad9a11d0a4f13487b2184f0b61fa8dc6
Visesh <viseshrprasad@gmail.com>
2016-12-01 19:28:35 -0500

    add Donor and Main Activity

---
+++ app/src/main/java/com/dbfall16/pblcloset/ui/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.dbfall16.pblcloset.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dbfall16.pblcloset.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
